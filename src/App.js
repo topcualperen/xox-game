6aff276d5802d2dcc28457753739ae416e2384c8
topcualperen <alperenttpc78@gmail.com>
2025-05-31 07:20:44 +0300

    v0.9 - First Playable Version

--- src/App.js
+++ src/App.js
@@ -8,16 +8,32 @@ const Square = ({value, onSquareClick}) => {
 
 const Board = () => {
 
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   const handleClick = (i) => {
+
+    if (calculateWinner(squares) || squares[i]) return;
+
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    
+    if (xIsNext) nextSquares[i] = "X";
+    else nextSquares[i] = "O";
+    setXIsNext(!xIsNext);
     setSquares(nextSquares);
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Kazanan: ' + winner;
+  } else {
+    status = 'Sıradaki oyuncu: ' + (xIsNext ? 'X' : 'O');
+  }   
+
   return (
     <div>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick= {() => handleClick(0)}/>
         <Square value={squares[1]} onSquareClick= {() => handleClick(1)}/>
@@ -37,3 +53,23 @@ const Board = () => {
   );
 }
 export default Board;
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
